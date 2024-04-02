06b32a93e4297268a70ff7e6cce7e6c370f0d37d
jenifferteixeira <jeniteixeira@outlook.com.br>
2024-04-02 11:17:53 -0300

    primeiro projeto react

---
+++ primeiro-projeto-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+import { ThemeProvider } from 'styled-components';
+import { NavBar } from './components/navbar';
+import { Screen } from './style';
+import { darkTheme, lightTheme } from './style/theme';
+
+function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <Screen>
+        <NavBar themeToggler={themeToggler} theme={theme}/>
+      </Screen>
+    </ThemeProvider>
+  );
+}
+
+export default App
---
+++ primeiro-projeto-react/src/components/navbar/index.jsx
@@ -0,0 +1,90 @@
+import { BsSun } from "react-icons/bs";
+import { BiMoon } from "react-icons/bi";
+import { AiOutLineHouse } from "react-icons/ai";
+import { MdOutlineSearch } from "react-icons/md";
+import { FiSend } from "react-icons/fi";
+import { GiClapperboard } from "react-icons/gi";
+import { BsGear } from "react-icons/bs";
+
+import LogoInstagram from "../../assets/logo-instagram.svg";
+
+import { Flex, Spacer, Typography } from "../../style";
+import * as C from "./style";
+
+const menuNav = [
+    {
+        icon: <AiOutLineHouse />,
+        menuName: 'Início'
+    },
+    {
+        icon: <MdOutlineSearch />,
+        menuName: 'Exploar'
+    },
+    {
+        icon: <FiSend />,
+        menuName: 'Direct'
+    },
+    {
+        icon: <GiClapperboard />,
+        menuName: 'Reels '
+    },
+    {
+        icon: <BsGear />,
+        menuName: 'Ajustes'
+    },
+]
+
+export function NavBar({ themeToggler, theme }) {
+    function GroupText({ title, subTitle }) {
+        return (
+            <Flex gap="4px">
+                <Typography>{title}</Typography>
+                <Typography weight="300" size="12px" height="14px">
+                    {subTitle}
+                </Typography>
+            </Flex>
+        );
+    }
+
+    return (
+        <C.Conteiner>
+            <Flex>
+                <C.BtnTheme onClick={themeToggler}>
+                    {theme === 'dark' ? <BsSun /> : <BiMoon />}
+                </C.BtnTheme>
+            </Flex>
+
+            <img src={LogoInstagram} alt="Logo Instagram" />
+
+            <Flex>
+                <C.Profile>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/157409713?v=4"
+                        alt="Imgame perfil"
+                    />
+                </C.Profile>
+
+                <GroupText title="Jeniffer Teixeira" subTitle="@oh.jeni" />
+            </Flex>
+
+            <Spacer margin="8px" />
+
+            <Flex direction="row" justify="space-between">
+                <GroupText title="222" subTitle="publicações" />
+                <GroupText title="413" subTitle="seguidores" />
+                <GroupText title="170" subTitle="seguindo" />
+            </Flex>
+
+            <Spacer />
+
+            <Flex align="start" gap="16px">
+                {menuNav.map((menu) => (
+                    <C.ListIcon key={menu.menuName}>
+                        {menu.icon}
+                        <Typography>{menu.menuName}</Typography>
+                    </C.ListIcon>
+                ))}
+            </Flex>
+        </C.Conteiner>
+    );
+}
